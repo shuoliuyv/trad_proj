28eac538aecb0543fc52256ef84f1c140ae90ae9
Shuo Liu <shuoliu@uchicago.edu>
2026-04-05 16:49:52 -0500

    ini

---
+++ p1_code.cpp
@@ -0,0 +1,5 @@
+void multiply_mv_row_major(const double* matrix, int rows, int cols, const double* vector, double* result);
+
+void multiply_mv_col_major(const double* matrix, int rows, int cols, const double* vector, double* result);
+
+void multiply_mm_naive(const double* matrixA, int rowsA, int colsA, const double* matrixB, int rowsB, int colsB, double* result);
